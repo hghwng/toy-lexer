8979479579376678756b16f0b8c186493556271a
Hugh Wang <hghwng@gmail.com>
2016-11-01 09:30:02 +0800

    Initial commit.

---
+++ fsa.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+from copy import deepcopy
+
+
+class Edge:
+    def __init__(self, dst, val):
+        self.val = val
+        self.dst = dst
+
+
+class State:
+    def __init__(self):
+        self.edges = list()
+
+
+class FSA:
+    def __init__(self):
+        self.finals = list()
+        self.states = [State()]
+
+    def add_final(self, index):
+        self.finals.append(index)
+
+    def add_state(self) -> int:
+        self.states.append(State())
+        return len(self.states) - 1
+
+    def add_final_state(self) -> int:
+        state = self.add_state()
+        self.add_final(state)
+        return state
+
+    def add_edge(self, src, dst, val):
+        self.states[src].edges.append(Edge(dst, val))
+
+    def add_edge_epsilon(self, src, dst):
+        self.add_edge(src, dst, 0)
+
+    def combine(self, fsa):
+        offset = len(self.states)
+        tmpfsa = fsa.duplicate()
+        for state in tmpfsa.states:
+            for edge in state.edges:
+                edge.dst += offset
+        for idx in range(len(tmpfsa.finals)):
+            tmpfsa.finals[idx] += offset
+        self.states.extend(tmpfsa.states)
+        self.finals.extend(tmpfsa.finals)
+        return offset
+
+    def duplicate(self):
+        return deepcopy(self)
+
+    def dump(self, f):
+        f.write('digraph {\n')
+        for final in self.finals:
+            f.write('\t' + str(final) + '[shape="box"]\n')
+
+        for state_index, state in enumerate(self.states):
+            for edge in state.edges:
+                disp = '(eps)' if edge.val == 0 else edge.val
+                f.write('\t' + str(state_index) + ' -> ' + str(edge.dst)
+                        + ' [label="' + disp + '"];\n')
+        f.write('}\n')
---
+++ regex.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+from fsa import FSA
+
+# simple: char | range | '(' regexp ')'
+#
+# repeating: simple '*'
+#          | simple '+'
+#          | simple '?'
+#          | simple
+#
+# sequence: repeating
+#         | repeating sequence
+#
+# regexp: sequence '|' regexp
+#       | sequence
+
+
+class _Parser:
+    FORBIDDEN_CHAR = "+*?|()[]"
+
+    def parse(self, regex: str):
+        self.pos = 0
+        self.regex = regex
+        self.maxpos = len(self.regex)
+        return self.parse_regexp()
+
+    def peek(self):
+        if self.pos < len(self.regex):
+            return self.regex[self.pos]
+        return chr(0)
+
+    def parse_char(self):
+        if self.peek() in self.FORBIDDEN_CHAR:
+            raise SyntaxError("Unexpected symbol " + self.peek())
+
+        char = self.regex[self.pos]
+        if char == '\\':
+            char = {'r': '\r', 'n': '\r', 'v': '\v'}.get(
+                self.regex[self.pos + 1], self.regex[self.pos + 1])
+            self.pos += 1
+
+        self.pos += 1
+        return char
+
+    def parse_range(self):
+        if self.regex[self.pos] != '[':
+            return None
+        self.pos += 1
+
+        fsa = FSA()
+        final = fsa.add_final_state()
+
+        while self.pos < self.maxpos:
+            if self.peek() == ']':
+                self.pos += 2
+                return fsa
+            char = self.parse_char()
+            if self.peek() == '-':
+                self.parse_char()
+                next_char = self.parse_char()
+                if ord(next_char) >= ord(char):
+                    for i in range(ord(char), ord(next_char) + 1):
+                        fsa.add_edge(0, final, chr(i))
+            else:
+                fsa.add_edge(0, final, char)
+        raise SyntaxError("Missing ]")
+
+    def parse_simple(self):
+        # Situation: '(' regexp ')'
+        if self.peek() == '(':
+            pos = self.pos
+            self.pos += 1
+            if self.peek() == ')':
+                self.pos += 1
+            else:
+                fsa = self.parse_regexp()
+                if fsa:
+                    if self.peek() != ')':
+                        raise SyntaxError("Missing )")
+                    else:
+                        self.pos += 1
+                        return fsa
+                else:
+                    self.pos = pos
+
+        # Situation: range
+        fsa = self.parse_range()
+        if fsa:
+            return fsa
+
+        # Situation: char
+        fsa = FSA()
+        final = fsa.add_final_state()
+        fsa.add_edge(0, final, self.parse_char())
+        return fsa
+
+    def parse_repeating(self):
+        subfsa = self.parse_simple()
+        fsa = subfsa.duplicate()
+        if self.peek() in "*?":
+            fsa.add_edge_epsilon(0, fsa.finals[0])
+        if self.peek() in "*+":
+            dup_begin = fsa.combine(subfsa)
+            dup_end = fsa.finals.pop()
+            fsa.add_edge_epsilon(fsa.finals[0], dup_begin)
+            fsa.add_edge_epsilon(dup_end, fsa.finals[0])
+        if self.peek() in "*+?":
+            self.pos += 1
+        return fsa
+
+    def parse_sequence(self):
+        fsa = None
+        while self.pos < self.maxpos:
+            if self.peek() in '|)':
+                return fsa
+            subfsa = self.parse_repeating()
+            if fsa:
+                subfsa_begin = fsa.combine(subfsa)
+                subfsa_end = fsa.finals.pop()
+                fsa.add_edge_epsilon(fsa.finals[0], subfsa_begin)
+                fsa.finals[0] = subfsa_end
+            else:
+                fsa = subfsa
+        return fsa
+
+    def parse_regexp(self):
+        fsa = FSA()
+        final = fsa.add_final_state()
+
+        while self.pos < self.maxpos:
+            subfsa = self.parse_sequence()
+            if subfsa is None:
+                continue
+            subfsa_begin = fsa.combine(subfsa)
+            subfsa_end = fsa.finals.pop()
+            fsa.add_edge_epsilon(0, subfsa_begin)
+            fsa.add_edge_epsilon(subfsa_end, final)
+
+            if self.peek() == '|':
+                self.pos += 1
+                continue
+            if self.peek == chr(0) or self.peek() in self.FORBIDDEN_CHAR:
+                return fsa
+
+        return fsa
+
+
+def parse(regex: str) -> FSA:
+    parser = _Parser()
+    return parser.parse(regex)
+
+
+def main():
+    import sys
+    fsa = parse(sys.argv[1])
+    fsa.dump(open('dump.dot', 'w'))
+
+if __name__ == '__main__':
+    main()
